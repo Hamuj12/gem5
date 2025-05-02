51c1ed1a5b1837edbd2d654a7fc62e18fe5a95fe
Hamza Mujtaba <hamuj12@cs.utexas.edu>
2025-05-01 22:53:56 -0500

    stats time

--- src/cpu/o3/lsq_unit.cc
+++ src/cpu/o3/lsq_unit.cc
@@ -204,7 +204,7 @@ LSQUnit::completeDataAccess(PacketPtr pkt)
                         tid, inst->seqNum, inst->pcState().instAddr(),
                         inst->pcState().microPC());
                 // This is the central place to handle mispredictions
-                handleValueMisprediction(inst);
+                // handleValueMisprediction(inst);
             } else {                        
                 // If this load has been classified as constant (high confidence prediction)
                 // and the prediction was correct, update the CVU
